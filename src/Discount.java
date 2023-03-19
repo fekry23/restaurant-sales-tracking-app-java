0b77e8eb7ed7cfd38d30a8d79d8c4c0f6e4b75c3
Fekry <73611267+fekry23@users.noreply.github.com>
2023-03-19 17:03:22 +0800

    Add files via upload

---
+++ src/Discount.java
@@ -0,0 +1,10 @@
+
+public class Discount {
+
+	public String[] discount = {"None","10%","20%"};
+	
+	public String[] getDiscount() {
+		return discount;
+	}
+	
+}
---
+++ src/Menu.java
@@ -0,0 +1,6 @@
+
+interface Menu {
+	public String[] getID();
+	public String[] getName();
+	public String[] getPrice();
+}
